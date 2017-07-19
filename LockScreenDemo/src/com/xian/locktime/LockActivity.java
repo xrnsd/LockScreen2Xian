29c3e1187c99d82a35b908d308e05bce0e196e41
wgx <lz_wuguoxian@163.com/15019962261/qq:1718686576>
2017-07-19 19:02:28 +0800

    添加样式支持

--- LockScreenDemo/src/com/xian/locktime/LockActivity.java
+++ LockScreenDemo/src/com/xian/locktime/LockActivity.java
@@ -3,14 +3,14 @@ package com.xian.locktime;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.xian.locktime.R;
-
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources.Theme;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
@@ -56,8 +56,8 @@ public class LockActivity extends Activity {
         getWindow().setAttributes(params);
         
         initUiState(getApplicationContext());
-        setContentView(R.layout.activity_main);
-        initTextTime(getApplicationContext(),0);
+        setContentView(R.layout.activity_main); 
+        initTextTime(getApplicationContext(),R.style.font_style_486_port);
     }
     
     @Override
@@ -81,8 +81,6 @@ public class LockActivity extends Activity {
         showLockScreen(getApplicationContext(),TIME_LONG);
     }
 
-
-
     private void initTextTime(Context context,int style_id){
         tvDate= (TextView) findViewById(R.id.tv_date);
         tvTime= (TextView) findViewById(R.id.tv_time);
@@ -90,19 +88,11 @@ public class LockActivity extends Activity {
             Utils.e(TAG," initTextTime fail textview is null" );
             return;
          }
-//      String fontFilePath="fonts/486.ttf";
-        String fontFilePath="fonts/digit.ttf";
-//      String fontFilePath="fonts/digigraphics.ttf";
-        Typeface fontFace = Typeface.createFromAsset(getAssets(),fontFilePath);
-        tvDate.setTypeface(fontFace);
-        tvTime.setTypeface(fontFace);
         
         if(style_id<=0)
             Utils.e(TAG, "initTextTime style fail style_id="+style_id);
-         else{
-             tvDate.setTextAppearance(getApplicationContext(), style_id);
-             tvTime.setTextAppearance(getApplicationContext(), style_id);
-         }
+         else
+             new ControlAttributes(context, style_id).applyStyle(tvDate,tvTime);
     }
     
     private void updateTime(Context context){
@@ -115,7 +105,7 @@ public class LockActivity extends Activity {
         String strDate=formatDate.format(new Date(time));
         tvDate.setText(strDate);
 
-        SimpleDateFormat formatTime=new SimpleDateFormat("HH :mm");
+        SimpleDateFormat formatTime=new SimpleDateFormat("HH mm");
         String strTime=formatTime.format(new Date(time));
         tvTime.setText(strTime);
     }
@@ -169,18 +159,76 @@ public class LockActivity extends Activity {
             policyManager.lockNow();
         }
     }
+    
+    public class ControlAttributes{
+        final String mFontFilePathStringDef="null";
+        final float mFontSizeDateDef=-1;
+        final float mFontSizeTimeDef=-1;
+        final int mFontColorDateDef=0xffffffff;
+        final int mFontColorTimeDef=0xffffffff;
+        
+        Theme mTheme;
+        Context mContext;
+        
+        String mFontFilePathString=mFontFilePathStringDef;
+        float mFontSizeDate=mFontSizeDateDef;
+        float mFontSizeTime=mFontSizeTimeDef;
+        int mFontColorDate=mFontColorDateDef;
+        int mFontColorTime=mFontColorTimeDef;
+        
+        
+        public ControlAttributes(Context context,int style_id) {
+            mContext=context;
+            mTheme = context.getResources().newTheme();
+            mTheme.applyStyle(style_id, true);
+            
+            TypedArray ta = mTheme.obtainStyledAttributes(R.styleable.LockScreenCtrol);
+            mFontFilePathString = ta.getString(R.styleable.LockScreenCtrol_fontFilePath);
+            mFontSizeDate=ta.getDimension(R.styleable.LockScreenCtrol_fontSize2Date, mFontSizeDateDef);
+            mFontSizeTime=ta.getDimension(R.styleable.LockScreenCtrol_fontSize2Time, mFontSizeTimeDef);
+            mFontColorDate=ta.getColor(R.styleable.LockScreenCtrol_fontColor2Date, mFontColorDateDef);
+            mFontColorTime=ta.getColor(R.styleable.LockScreenCtrol_fontColor2Time, mFontColorTimeDef);
+            
+            ta.recycle();
+        }
+        
+        public void applyStyle(TextView ...tv_list) {
+            android.util.Log.d("123456", "LockActivity_applyStyle mFontFilePathString="+mFontFilePathString);
+            Typeface fontFace = Typeface.createFromAsset(getAssets(),mFontFilePathString);
+            for (TextView tv : tv_list) {
+                switch (tv.getId()) {
+                case R.id.tv_date:
+                    android.util.Log.d("123456", "tv mFontColorDate="+mFontColorDate);
+                    if(mFontSizeDateDef!=mFontSizeDate)
+                        tv.setTextSize(mFontSizeDate);
+                    tv.setTextColor(mFontColorDate);
+                    break;
+                case R.id.tv_time:
+                    android.util.Log.d("123456", "tv mFontColorTime="+mFontColorTime);
+                    if(mFontSizeTimeDef!=mFontSizeTime)
+                        tv.setTextSize(mFontSizeTime);
+                    tv.setTextColor(mFontColorTime);
+                    break;
+                default:
+                    break;
+                }
+                if(mFontFilePathStringDef!=mFontFilePathString)
+                    tv.setTypeface(fontFace);
+            }
+        }
+    }
 
     public  static class Utils{
         private final static boolean IS_DEBUG=true;
         public static void e(String tag,String content) {
             if(IS_DEBUG){
-                tag+=PROJECT_NAME;
+                tag=PROJECT_NAME+"__"+tag;
                 android.util.Log.e(tag,content);
             }
         }
         public static void d(String tag,String content) {
             if(IS_DEBUG){
-                tag+=PROJECT_NAME;
+                tag=PROJECT_NAME+"__"+tag;
                 android.util.Log.d(tag,content);
             }
         }
